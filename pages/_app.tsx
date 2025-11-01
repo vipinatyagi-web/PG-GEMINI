3f1091e5202dba1613e00caca51c18154deabd1a
vipinatyagi-web <vipin.a.tyagi@gmail.com>
2025-11-01 04:29:40 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// pages/_app.js
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
