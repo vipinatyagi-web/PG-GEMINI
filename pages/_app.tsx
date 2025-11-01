3f37a5e145ad475083bf01730841319e93199d9f
vipinatyagi-web <vipin.a.tyagi@gmail.com>
2025-11-01 10:02:14 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-// pages/_app.js
-import "../styles/globals.css";
+// pages/_app.tsx (Poora aur Sahi Code)
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app' // Yeh line zaroori hai
+
+// Yahaan par `: AppProps` hi asli fix hai
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
